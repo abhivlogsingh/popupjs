d50602e5460aab5a0a814cc056c424c7b30d3dc2
Abhishek Singh <73503674+abhivlogsingh@users.noreply.github.com>
2023-06-04 12:02:41 +0530

    Add files via upload

---
+++ js popup/script.js
@@ -0,0 +1,6 @@
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', showMsg);
+
+function showMsg(){
+    alert("Namaste Abhishek  !!  Today work is pratice js 4jun");
+}
